40334f4f2027b57a58f63bfb8faa80b9a7050a1b
Raj Jagrup <rajjagrup1@gmail.com>
2023-04-12 16:46:10 -0400

    Updated the index.php

--- index.php
+++ index.php
@@ -1,63 +1,32 @@
 <?php
 
-    // Connect to the localhost
+// Define database connection variables
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "inventory";
 
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname   =  "myDB";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    //Makes the connects to the database
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    $conn =  mysqli_connect($servername, $username, $password, $dbname);
-    
-    //Checks the connect 
-    if($conn->connect_error)
-    {
-        die("Connections Failed" .$conn->connect_error);
-    }
+// Insert sample data
+$sql = "INSERT INTO products (name, description, quantity, price)
+VALUES ('Product 1', 'Description 1', 10, 9.99),
+       ('Product 2', 'Description 2', 5, 19.99),
+       ('Product 3', 'Description 3', 2, 29.99)";
 
-    //Stores the data inputs
-    $sql = "Insert the vaules given()";
+if ($conn->query($sql) === TRUE) {
+    echo "Sample data inserted successfully";
+} else {
+    echo "Error inserting sample data: " . $conn->error;
+}
 
-    //Check to see if data was added 
-    if(mysqli_query($conn, $sql))
-        {
-            echo "New record has been added!";        
-        }
-    else
-        {
-            echo  "Error:  " .$sql."<br>" .mysqli_error($conn); 
-        }
+// Close connection
+$conn->close();
 
-    
-   mysqli_close($conn);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css">
-    <title>PHP Form Demo</title>
-</head>
-<body>
-    <main>
-        <form action="insert.php" method="post">
-            <div>
-                <label for="name">Name:</label>
-                <input type="text" name="name" required="required" placeholder="Enter your name" />
-            </div>
-
-            <div>
-                <label for="name">Email:</label>
-                <input type="email" name="email" required="required" placeholder="Enter your email" />
-            </div>
-
-            <button type="submit">Subscribe</button>
-        </form>
-    </main>
-</body>
-</html>
-
